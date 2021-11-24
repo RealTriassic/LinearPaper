666559c7a96cba9b5c30f8c8165568aa9d0f4828
Encode42 <me@encode42.dev>
2021-11-23 22:43:25 -0500

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    pl3xgaming/Purpur@8bb5a93 Ignore structure e seeds if set to -1
    pl3xgaming/Purpur@7037004 [ci-skip] remove last mention of jenkins in README
    pl3xgaming/Purpur@9beb23b Final 1.17 Upstream Update (Paper)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,24 @@
+import io.papermc.paperweight.util.constants.*
+
 plugins {
     java
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.14"
+    id("io.papermc.paperweight.patcher") version "1.2.0"
 }
 
 repositories {
     mavenCentral()
     maven("https://papermc.io/repo/repository/maven-public/") {
         content {
-            onlyForConfigurations("paperclip")
+            onlyForConfigurations(PAPERCLIP_CONFIG)
         }
     }
 }
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.12")
     paperclip("io.papermc:paperclip:2.0.1")
 }
 
@@ -48,6 +52,9 @@ subprojects {
 paperweight {
     serverProject.set(project(":Tentacles-Server"))
 
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
     useStandardUpstream("purpur") {
         url.set(github("pl3xgaming", "Purpur"))
         ref.set(providers.gradleProperty("purpurCommit"))
@@ -57,9 +64,6 @@ paperweight {
 
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("Tentacles-API"))
-
-            remapRepo.set("https://maven.fabricmc.net/")
-            decompileRepo.set("https://files.minecraftforge.net/maven/")
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Tentacles-Server"))
