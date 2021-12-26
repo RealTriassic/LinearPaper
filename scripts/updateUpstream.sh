6aca99cc513516fb89334a19ccd02147fd752613
Encode42 <me@encode42.dev>
2021-12-25 23:27:10 -0500

    updateUpstream script

---
+++ scripts/updateUpstream.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# file utilized in github actions to automatically update upstream
+
+(
+set -e
+PS1="$"
+
+current=$(cat gradle.properties | grep purpurCommit | sed 's/purpurCommit = //')
+upstream=$(git ls-remote https://github.com/PurpurMC/Purpur | grep HEAD | cut -f 1)
+
+if [ "$current" != "$upstream" ]; then
+    sed -i 's/purpurCommit = .*/purpurCommit = '"$upstream"'/' gradle.properties
+    ./gradlew applyPatches --stacktrace && {
+        ./gradlew build --stacktrace && {
+            ./gradlew rebuildPatches --stacktrace
+            git add .
+            ./scripts/upstreamCommit.sh "$current"
+        }
+    }
+fi
+
+) || exit 1
