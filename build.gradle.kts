49e2450b57995d6d0d2a6b4563a9266ee4454f2f
5jd <85830207+5jd@users.noreply.github.com>
2022-11-06 22:05:05 +0100

    Fix upstream updates (#7)
    * Update build.gradle.kts
    
    * Update build.yml dependencies
    
    * Update upstream.yml dependencies
    
    * Updated Upstream (Purpur)
    
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    PurpurMC/Purpur@cb121ec Updated Upstream (Paper)
    PurpurMC/Purpur@d43289c [ci-skip] only build on main branch
    PurpurMC/Purpur@d12a88e Fix #1181 (#1182)
    PurpurMC/Purpur@0b68340 Updated Upstream (Paper)
    PurpurMC/Purpur@6e7a51e helps to include this bit
    
    * Don't specify minor versions in workflows
    
    Github actions get the latest minor versions of dependencies. Thanks to @Encode42 for letting me know that.
    
    Co-authored-by: Github Action <action@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.8"
+    id("io.papermc.paperweight.patcher") version "1.3.9"
 }
 
 repositories {
@@ -17,8 +17,8 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.2:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.605.9")
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.605.10")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
