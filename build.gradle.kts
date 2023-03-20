397a3b4437db5c1536de793677bf60104e4c04ca
Encode42 <me@encode42.dev>
2023-03-20 13:54:17 -0400

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    PurpurMC/Purpur@c5f1057 Updated Upstream (Paper)
    PurpurMC/Purpur@3940c82 Add PreExplodeEvents (#1234)
    PurpurMC/Purpur@ade1ed1 Updated Upstream (Paper)
    PurpurMC/Purpur@ef4b73c Updated Upstream (Paper)
    PurpurMC/Purpur@9e436f7 Updated Upstream (Paper)
    PurpurMC/Purpur@235d507 Updated Upstream (Paper & Pufferfish)
    PurpurMC/Purpur@890a9d7 inline Component import
    PurpurMC/Purpur@8c8c18b Updated Upstream (Paper)
    PurpurMC/Purpur@db11753 Fix NPE when no world given to explosions (#1286)
    PurpurMC/Purpur@ea72a33 Update Rhino to 1.7.14 (#1285)
    PurpurMC/Purpur@72b7579 Add option to fix creative mode actions with NetworkItemSerializeEvent (#1168)
    PurpurMC/Purpur@67066cd [ci-skip] remove unused config option
    PurpurMC/Purpur@1dea120 finish applying patches for 1.19.4
    PurpurMC/Purpur@6314bdd fix some compile errors
    PurpurMC/Purpur@3c0d4f1 fix some compile errors
    PurpurMC/Purpur@55dbafe it compiles \o/
    PurpurMC/Purpur@d383dbb use magic damage type instead of custom
    PurpurMC/Purpur@56f5adf fix paperclip bug
    PurpurMC/Purpur@389fb6b fix some patches
    PurpurMC/Purpur@95ad375 fix endermen projectile issues
    PurpurMC/Purpur@4715a7e [ci-skip] add more stuff to todo list
    PurpurMC/Purpur@808b1cb flip some config defaults for vanilla parity
    PurpurMC/Purpur@09b5d54 Revert "flip some config defaults for vanilla parity"
    PurpurMC/Purpur@5200826 flip some config defaults for vanilla parity
    PurpurMC/Purpur@ab3bcc9 Improve JS exec return value handling (#1290)
    PurpurMC/Purpur@ea899ca make sniffer ridable
    PurpurMC/Purpur@abe0fb1 [ci-skip] update todo file

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,27 +3,13 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.11"
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.2"
 }
 
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-            onlyForConfigurations(PAPERCLIP_CONFIG)
-        }
-    }
-}
-
-dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.605.1")
-    paperclip("io.papermc:paperclip:3.0.2")
-}
-
-subprojects {
+allprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     java {
         toolchain {
@@ -32,39 +18,47 @@ subprojects {
     }
 }
 
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
 subprojects {
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
-
     tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
     }
-
     tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
 
     repositories {
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven(paperMavenPublicUrl)
         maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
+}
 
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content {
+            onlyForConfigurations(configurations.paperclip.name)
+        }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    paperclip("io.papermc:paperclip:3.0.2")
 }
 
 paperweight {
     serverProject.set(project(":tentacles-server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    remapRepo.set(paperMavenPublicUrl)
+    decompileRepo.set(paperMavenPublicUrl)
 
     useStandardUpstream("purpur") {
         url.set(github("PurpurMC", "Purpur"))
@@ -79,5 +73,48 @@ paperweight {
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Tentacles-Server"))
         }
+    }
+}
+
+tasks.generateDevelopmentBundle {
+    apiCoordinates.set("org.purpurmc.tentacles:tentacles-api")
+    mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
+    libraryRepositories.set(
+        listOf(
+            "https://repo.maven.apache.org/maven2/",
+            paperMavenPublicUrl,
+            "https://repo.purpurmc.org/snapshots",
+        )
+    )
+}
+
+allprojects {
+    publishing {
+        repositories {
+            maven("https://repo.purpurmc.org/snapshots") {
+                name = "tentacles"
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("devBundle") {
+        artifact(tasks.generateDevelopmentBundle) {
+            artifactId = "dev-bundle"
+        }
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    doLast {
+        println(providers.gradleProperty("mcVersion").get().trim())
+    }
+}
+
+tasks.register("printPaperVersion") {
+    doLast {
+        println(project.version)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,32 @@ import java.util.Locale
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Tentacles project directory is not a properly cloned Git repository.
+         
+         In order to build Tentacles from source you must clone
+         the repository using Git, not download a code zip from GitHub.
+         
+         See https://github.com/PurpurMC/Purpur/blob/HEAD/CONTRIBUTING.md
+         for further information on building and modifying Purpur.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 rootProject.name = "tentacles"
+
 for (name in listOf("Tentacles-API", "Tentacles-Server")) {
     val projName = name.toLowerCase(Locale.ENGLISH)
     include(projName)
