02b0988e157a813031f0cd0241bb43aa6037809a
Triassic <techbyteofficial9@gmail.com>
2024-05-11 20:59:31 +0300

    Re-add Linear region format patch

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -9,13 +9,13 @@
 set -e
 PS1="$"
 
-purpur=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PurpurMC/Purpur/compare/$1...ver/1.20.6 | jq -r '.commits[] | "PurpurMC/Purpur@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+paper=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PaperMC/Paper/compare/$1...$2 | jq -r '.commits[] | "PaperMC/Paper@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
 
 updated=""
 logsuffix=""
-if [ ! -z "$purpur" ]; then
-    logsuffix="$logsuffix\n\nPurpur Changes:\n$purpur"
-    updated="Purpur"
+if [ ! -z "paper" ]; then
+    logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
+    updated="Paper"
 fi
 disclaimer="Upstream has released updates that appear to apply and compile correctly"
 
