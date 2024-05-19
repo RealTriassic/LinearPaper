4a974773a68347040386123e7ad3830d4e172993
Triassic <techbyteofficial9@gmail.com>
2024-05-19 11:39:27 +0300

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@d8d59e0 Fix NPE sending resource pack without prompt
    PaperMC/Paper@5a9afbe Fixes issues in the suppress dismount cancellation patch (#10735)
    PaperMC/Paper@0ad09de Make PaperSimplePluginClassLoader show class which is not found

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -2,14 +2,14 @@
 
 # requires curl & jq
 
-# upstreamCommit <baseHash> <HEAD>
+# upstreamCommit <baseHash>
 # param: bashHash - the commit hash to use for comparing commits (baseHash...HEAD)
 
 (
 set -e
 PS1="$"
 
-paper=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PaperMC/Paper/compare/$1...$2 | jq -r '.commits[] | "PaperMC/Paper@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+paper=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PaperMC/Paper/compare/$1...master | jq -r '.commits[] | "PaperMC/Paper@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
 
 updated=""
 logsuffix=""
